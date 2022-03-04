a776f0bea444776ddcee8adc7da2233d713cc2f1
Sevki Yontem <sevki@fireflyon.com>
2022-03-04 22:28:14 +0300

    cizdirme fix nerden geldigi belli olmayan hatayi...

--- backtest.py
+++ backtest.py
@@ -2,7 +2,7 @@ from trade_logic.utils import *
 from trade_logic.app import App
 
 if __name__ == '__main__':
-    baslangic_gunu = datetime.strptime('2022-01-01 00:00:00', '%Y-%m-%d %H:%M:%S')
+    baslangic_gunu = datetime.strptime('2021-10-01 00:00:00', '%Y-%m-%d %H:%M:%S')
     baslangic_gunu = baslangic_gunu.replace(tzinfo=None)
     app = App(baslangic_gunu)
 
