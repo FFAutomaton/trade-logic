b99e75b471504ce0ce1d4788604dbcdcca82ce0b
Sevki Yontem <sevki@fireflyon.com>
2022-05-11 19:07:34 +0300

    bambam test dosyasi

---
+++ bambam_test.py
@@ -0,0 +1,8 @@
+from service.bam_bam_service import bam_bama_sinyal_gonder
+
+
+if __name__ == '__main__':
+    islem = {}
+    islem["alis"] = 1000
+    yon = -1
+    bam_bama_sinyal_gonder(islem, yon)
