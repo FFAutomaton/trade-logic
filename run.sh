698bdbbeb08089a39aaba2584f287594e564155a
sevkibaba <sevkiyonte@gmail.com>
2022-07-22 17:09:26 +0300

    veritabani guncelle, yarim saatte bir calis

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 timestamp=`date +%Y/%m/%d-%H:%M:%S`
-echo "========================== Bot calisiyor $timestamp =========================="
+echo "                                                            Bot calisiyor $timestamp =========================="
 
 python3 -u /app/main.py
