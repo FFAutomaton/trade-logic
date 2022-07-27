3467709172cfe217c2f1d567d91794555d5a938c
sevkibaba <sevkiyonte@gmail.com>
2022-07-28 00:11:26 +0300

    fix rebuild.sh

--- bash_scripts/rebuild.sh
+++ bash_scripts/rebuild.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 DOCKER_ID=$(docker ps -aqf "name=prophet-trader")
 docker stop $DOCKER_ID
-docker build --rm -t prophet-trader ../
+docker build --rm -t prophet-trader ./
 docker run -t -i -d --rm \
     --name prophet-trader -v /home/sevki/Documents/repos/trade-logic/trade-bot-logs:/output \
      -v /home/sevki/Documents/repos/trade-logic/coindata/ETHUSDT.db:/app/coindata/ETHUSDT.db \
