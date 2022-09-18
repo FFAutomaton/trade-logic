eb5b895de536ef279bce28ecb3ed78c837dfe792
Sevki Yontem <sevki@fireflyon.com>
2022-09-19 01:09:38 +0300

    test aws cron

--- bash_scripts/rebuild.sh
+++ bash_scripts/rebuild.sh
@@ -4,6 +4,6 @@ DOCKER_ID=$(docker ps -aqf "name=trade-bot")
 docker stop $DOCKER_ID
 docker build --rm -t trade-bot ./
 docker run -t -i -d --rm \
-    --name trade-bot -v /Users/sevki/Documents/repos/turkish-gekko-organization/trade-logic/trade-bot-logs:/output \
-     -v /Users/sevki/Documents/repos/turkish-gekko-organization/trade-logic/coindata/ETHUSDT.db:/app/coindata/ETHUSDT.db \
+    --name trade-bot -v /home/ubuntu/trade-logic/trade-bot-logs:/output \
+     -v /home/ubuntu/trade-logic/coindata/ETHUSDT.db:/app/coindata/ETHUSDT.db \
      trade-bot
